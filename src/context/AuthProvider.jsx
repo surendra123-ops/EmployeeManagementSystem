29b22fa95cbbcd20d49e47db2ae880e368f720be
surendra <surendrakumarreddy79@gmail.com>
2025-02-01 22:18:41 +0530

    Initial commit

---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,22 @@
+import React, { createContext, useEffect, useState } from 'react'
+import { getLocalStorage, setLocalStorage } from '../utils/localStorage'
+
+export const AuthContext = createContext()
+
+const AuthProvider = ({ children }) => {
+    const [userData, setUserData] = useState(null)
+
+    useEffect(() => {
+        setLocalStorage()
+        const { employees } = getLocalStorage()
+        setUserData(employees)
+     }, [])
+
+    return (
+        <AuthContext.Provider value={[userData, setUserData]}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthProvider
